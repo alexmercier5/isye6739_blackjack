4fddc99fb81ffba1d2748cad8005855e6f2c0144
Alex Mercier <alexmercier@alex-macbook.local>
2025-05-29 02:25:42 -0400

    Couple of fixes can be seen in todo at top....getting closer

--- main.py
+++ main.py
@@ -19,37 +19,45 @@ Insurance:
 If the dealer's face-up card is an Ace, you can bet on whether they have Blackjack. This bet pays 2:1 if the dealer does have Blackjack but loses otherwise, according to the Venetian Las Vegas. 
 Push:
 If you and the dealer have the same hand value, it's a push (tie), and your bet is returned to you. 
+
+EXTRA:
+Determine strategies, correct number of cards in deck changing, when player delt 8 and ace, player hits but says bust when the ace should go to 1 and keep playing 
 '''
 
-def setup(bankroll=100):
-    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-    values = {
-        '2': 2,
-        '3': 3,
-        '4': 4,
-        '5': 5,
-        '6': 6,
-        '7': 7,
-        '8': 8,
-        '9': 9,
-        '10': 10,
-        'Jack': 10,
-        'Queen': 10,
-        'King': 10,
-        'Ace': 11
-    }
+def setup(bankroll=100, deck=None):
+    if deck is None:
+        print("Setting up the game with a new deck of cards...\n")
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        values = {
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9,
+            '10': 10,
+            'Jack': 10,
+            'Queen': 10,
+            'King': 10,
+            'Ace': 11
+        }
 
-    deck = []
-    for i in range(6):  # 6 decks
-        for suit in suits:
-            for rank in ranks:
-                deck.append((rank, suit, values[rank]))
+        deck = []
+        for i in range(6):  # 6 decks
+            for suit in suits:
+                for rank in ranks:
+                    deck.append((rank, suit, values[rank]))
 
-    print(f"Deck size: {len(deck)}\n\n")
-    # print("Deck of cards:")
-    # for card in deck:
-    #     print(f"{card[0]} of {card[1]} (Value: {card[2]})")
+        print(f"Deck size: {len(deck)}\n\n")
+        # print("Deck of cards:")
+        # for card in deck:
+        #     print(f"{card[0]} of {card[1]} (Value: {card[2]})")
+    else:
+        print("Using the existing deck of cards...\n")
+        print(f"Deck size: {len(deck)}\n\n")
 
     return deck, bankroll
 
@@ -59,42 +67,48 @@ def shuffle_deck(deck):
     random.shuffle(deck)
     return deck
 
-def dealer_hand(hand, deck):
+def dealer_hand(hand, deck, ifBusted=False):
     """Deal a hand of cards to the dealer."""
     if hand is None:
         hand = []
-        for dealer_card in range(2):
+        card = deck.pop()
+        hand.append(card)
+        if sum(card[2] for card in hand) > 21:
+            for each_card in hand:
+                if each_card[0] == 'Ace':
+                    hand.remove(each_card)
+                    hand.append(('Ace', each_card[1], 1))
+                    break
+        for i, each_card in enumerate(deck):
+            if each_card[0] == card[0] and each_card[1] == card[1]:
+                del deck[i]
+                break
+        print(f"Dealer dealt: xxx of xxx (Hidden)")
+    else:
+        if len(hand) == 1:
             card = deck.pop()
             hand.append(card)
-            if sum(card[2] for card in hand) > 21:
-                for each_card in hand:
-                    if each_card[0] == 'Ace':
-                        hand.remove(each_card)
-                        hand.append(('Ace', each_card[1], 1))
-                        break
+            print(f"Dealer dealt: {card[0]} of {card[1]}")
             for i, each_card in enumerate(deck):
                 if each_card[0] == card[0] and each_card[1] == card[1]:
                     del deck[i]
                     break
-            if dealer_card == 0:
-                print(f"Dealer dealt: xxx of xxx (Hidden)")
-            else:
-                print(f"Dealer dealt: {card[0]} of {card[1]}\n\n")
-    else:
-        print("\n\n")
-        for card in hand:
-            print(f"Dealer's hand: {card[0]} of {card[1]}")
-        while sum(card[2] for card in hand) <= 16:
-            card = deck.pop()
-            hand.append(card)
-            print(f"Dealer hits and is dealt: {card[0]} of {card[1]}")
-            if sum(card[2] for card in hand) > 21:
-                for each_card in hand:
-                    if each_card[0] == 'Ace':
-                        hand.remove(each_card)
-                        hand.append(('Ace', each_card[1], 1))
-                        break
         else:
+            if ifBusted:
+                pass
+            else:
+                for card in hand:
+                    print(f"Dealer's hand: {card[0]} of {card[1]}")
+                while sum(card[2] for card in hand) <= 16:
+                    card = deck.pop()
+                    hand.append(card)
+                    print(f"Dealer hits and is dealt: {card[0]} of {card[1]}")
+                    if sum(card[2] for card in hand) > 21:
+                        for each_card in hand:
+                            if each_card[0] == 'Ace':
+                                hand.remove(each_card)
+                                hand.append(('Ace', each_card[1], 1))
+                                break
             print("Dealer stands.")
     return hand, deck
 
@@ -104,6 +118,7 @@ def player_hand(deck, bankroll):
     print(f"Player's current balance: ${balance}\n")
     print(f"Enter your bet amount (or type 'exit' to quit): ")
     bet = input()
+    bet2 = 0
     if bet.lower() == 'exit':
         print("Exiting the game.")
         exit()
@@ -115,23 +130,34 @@ def player_hand(deck, bankroll):
     except ValueError:
         print("Invalid bet amount. Please enter a valid number.")
         return player_hand(deck, balance)
-    
-    #TODO: 
-    '''
-    Make sure that a card is removed from the deck after being delt on line 126, 
-    also need to deal in the order: player, dealer, player, dealer where 4 cards are delt before hit/stand is asked
-    '''
+    balance -= bet
+    print(f"Player's bet: ${bet}\n")
     hand = []
     split_hand = []
-    split_card_num = 0
     num_of_hands = 1
-    card = deck.pop()
-    hand.append(card)
-    print(f"Player dealt: {card[0]} of {card[1]}")
-    #dealer_hand(None, deck)
+    if len(hand) < 1:
+        card = deck.pop()
+        hand.append(card)
+        print(f"Player dealt: {card[0]} of {card[1]}")
+        for i, each_card in enumerate(deck):
+                    if each_card[0] == card[0] and each_card[1] == card[1]:
+                        del deck[i]
+                        break
+        dealer_hand_1card, deck = dealer_hand(None, deck)
+    if len(hand) == 1:
+        card = deck.pop()
+        hand.append(card)
+        print(f"Player dealt: {card[0]} of {card[1]}")
+        for i, each_card in enumerate(deck):
+            if each_card[0] == card[0] and each_card[1] == card[1]:
+                del deck[i]
+                break
+        dealer_hand_2card, deck = dealer_hand(dealer_hand_1card, deck)
     mainPlaying = True
     splitPlaying = False
     playerPlaying = True
+    turn = 0
+    busted = False
     while playerPlaying:
         for indiv_hand in range(num_of_hands):
             if indiv_hand == 0:
@@ -145,30 +171,37 @@ def player_hand(deck, bankroll):
                     continue
                 total_value = sum(card[2] for card in split_hand)
             print(f"\n\n{current_hand} --- Total: {total_value} \nHit or Stand? Type 'h' or enter to hit or type 's' to stand.\n")
+            if turn == 0:
+                print(f"Type 'double' or 'd' to double down.\n")
+            if card[2] == hand[0][2] and turn == 0:
+                print("\n\nPlayer has a pair! Type 'split' to split, else game continues\n")
             input_choice = input()
             if input_choice.lower() == 'h' or input_choice == '':
+                print(f"Player hits.\n")
                 card = deck.pop()
-                card=card
                 if indiv_hand == 0:
                     hand.append(card)
                 elif indiv_hand == 1:
                     split_hand.append(card)
                 print(f"\nPlayer dealt: {card[0]} of {card[1]}")
-                split_card_num += 1
-                if card[0] == hand[0][0] and split_card_num == 1:
-                    print("\n\nPlayer has a pair! Type 'split' to split, else game continues\n\n")
-                    split_choice = input()
-                    if split_choice.lower() == 'split':
-                        splitPlaying = True
-                        num_of_hands += 1
-                        split_hand.append(card)
-                        hand.remove(card)
-                if sum(card[2] for card in hand) > 21:
-                    for each_card in hand:
-                        if each_card[0] == 'Ace':
-                            hand.remove(each_card)
-                            hand.append(('Ace', each_card[1], 1))
-                            break
+            elif input_choice.lower() == 'split':
+                balance -= bet
+                bet2 = bet
+                print(f"Player splits the hand. New bet is ${bet} and ${bet2}.")
+                splitPlaying = True
+                num_of_hands += 1
+                split_hand.append(card)
+                hand.remove(card)
+            elif input_choice.lower() == 'double' or input_choice.lower() == 'd':
+                if balance >= bet:
+                    balance -= bet
+                    bet *= 2
+                    card = deck.pop()
+                    hand.append(card)
+                    print(f"Player doubled down and is dealt: {card[0]} of {card[1]}")
+                    playerPlaying = False
+                else:
+                    print("Insufficient balance to double down.")
             else:
                 if indiv_hand == 0:
                     mainPlaying = False
@@ -179,6 +212,7 @@ def player_hand(deck, bankroll):
                 total_value = sum(card[2] for card in hand)
                 if total_value > 21:
                     print(f"{current_hand}: Player busts!")
+                    busted = True
                     mainPlaying = False
                 elif total_value == 21:
                     print(f"{current_hand}: Player has blackjack!")
@@ -208,11 +242,13 @@ def player_hand(deck, bankroll):
                         break
         if not mainPlaying and not splitPlaying:
             playerPlaying = False
+        turn += 1
     if len(split_hand) == 0:
         split_hand = None
-    return hand, split_hand, deck
+    return hand, split_hand, deck, dealer_hand_2card, bet, bet2, balance, busted
 
-def compare_hands(dealer_hand, player_hand, split_hand):
+def compare_hands(dealer_hand, player_hand, split_hand, bet, bet2, balance):
+    profit = 0
     dealer_total = sum(card[2] for card in dealer_hand)
     player_total = sum(card[2] for card in player_hand)
 
@@ -226,14 +262,25 @@ def compare_hands(dealer_hand, player_hand, split_hand):
     print(f"Player's total value: {player_total}\n")
     if dealer_total > 21 and player_total <= 21:
         print("Player wins!\n")
+        profit = bet * 2
+        balance += profit
     elif player_total > 21:
         print("Dealer wins!\n")
+        profit = -bet
+        balance += profit
     elif dealer_total > player_total:
         print("Dealer wins!\n")
+        profit = -bet
+        balance += profit
     elif player_total > dealer_total:
         print("Player wins!\n")
+        profit = bet * 2
+        balance += profit
     else:
         print("It's a tie!\n")
+        profit = 0
+        balance += bet
+    print(f"Player's balance after this round: ${balance}\n")
 
     if split_hand:
         split_total = sum(card[2] for card in split_hand)
@@ -243,19 +290,31 @@ def compare_hands(dealer_hand, player_hand, split_hand):
         print(f"Player's split hand total value: {split_total}")
         if dealer_total > 21 and split_total <= 21:
             print("Player's split hand wins!\n\n")
+            profit += bet2 * 2
+            balance += bet2 * 2
         elif split_total > 21:
             print("Dealer beats split hand!\n\n")
+            profit -= bet2
+            balance -= bet2
         elif dealer_total > split_total:
             print("Dealer beats split hand!\n\n")
+            profit -= bet2
+            balance -= bet2
         elif split_total > dealer_total:
             print("Player's split hand wins!\n\n")
+            profit += bet2 * 2
+            balance += bet2 * 2
         else:
             print("Split hand ties dealer!\n\n")
+            profit += bet2
+            balance += bet2
+    revenue = profit - (bet + bet2)
+    return profit, balance, revenue
 
 def choose_strategy(deck):
     return "standard"
 
-def save_results(dealers_hand, players_hand, split_hand, strategy):
+def save_results(dealers_hand, players_hand, split_hand, strategy, profit, balance, revenue):
     dealer_total = sum(card[2] for card in dealers_hand)
     player_total = sum(card[2] for card in players_hand)
     if split_hand:
@@ -274,6 +333,9 @@ def save_results(dealers_hand, players_hand, split_hand, strategy):
         'Player Hand Value': player_total,
         'Split Hand': split_hand,
         'Split Hand Value': split_total,
+        'Profit': profit,
+        'Balance': balance,
+        'Revenue': revenue,
         'Strategy': strategy
     }
     result_df = pd.DataFrame([result_row])
@@ -283,10 +345,15 @@ def save_results(dealers_hand, players_hand, split_hand, strategy):
     return None
 
 if __name__ == "__main__":
-    deck, balance = setup(bankroll=100)
-    shuffled_deck = shuffle_deck(deck)
-    players_hand, split_hand, current_deck = player_hand(shuffled_deck, balance)
-    dealers_hand, current_deck = dealer_hand(None, current_deck)
-    dealers_hand, final_deck = dealer_hand(dealers_hand, current_deck)
-    compare_hands(dealers_hand, players_hand, split_hand)
-    save_results(dealers_hand, players_hand, split_hand, choose_strategy(final_deck))
+    balance = int(input("Enter your starting bankroll: "))
+    deck = None
+    while True:
+        deck, balance = setup(bankroll=balance, deck=deck)
+        shuffled_deck = shuffle_deck(deck)
+        players_hand, split_hand, current_deck, dealer_hands, bet, bet2, balance, ifBusted = player_hand(shuffled_deck, balance)
+        dealers_hand, final_deck = dealer_hand(dealer_hands, current_deck, ifBusted)
+        profit, balance, revenue = compare_hands(dealers_hand, players_hand, split_hand, bet, bet2, balance)
+        save_results(dealers_hand, players_hand, split_hand, choose_strategy(final_deck), profit, balance, revenue)
+        if balance <= 0:
+            print("You have run out of money! Game over.")
+            break
